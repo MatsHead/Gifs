7b862d90ad9d240f614a66ce34a5384ff41eaed2
Adrian Steć <stadkr@gmail.com>
2019-07-23 12:43:15 +0200

    add categories

--- src/main/java/pl/matshead/Gifs/Category.java
+++ src/main/java/pl/matshead/Gifs/Category.java
@@ -1,10 +1,42 @@
 package pl.matshead.Gifs;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class Categories {
-    List<String> categories = new ArrayList<>();
-    categories.add("programming");
+public class Category {
+    private Integer id;
+    private String name;
 
+    public static List<Category> getCategories(){
+        List<Category> categories = new ArrayList<>();
+        categories.add(new Category(1,"programming"));
+        categories.add(new Category(2,"bot"));
+        categories.add(new Category(3,"others"));
+
+        return categories;
+
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
