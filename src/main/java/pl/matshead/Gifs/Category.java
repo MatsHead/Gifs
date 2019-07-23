c41b3f097b5319e172158d3dfc9ff3aaba9a8efc
Adrian Steć <stadkr@gmail.com>
2019-07-23 12:41:12 +0200

    Merge branch 'master' of D:\projekty\Gifs with conflicts.

---
+++ src/main/java/pl/matshead/Gifs/Category.java
@@ -0,0 +1,10 @@
+package pl.matshead.Gifs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Categories {
+    List<String> categories = new ArrayList<>();
+    categories.add("programming");
+
+}
---
+++ src/main/java/pl/matshead/Gifs/controller/CategoryController.java
@@ -0,0 +1,4 @@
+package pl.matshead.Gifs.controller;
+
+public class CategoryController {
+}
