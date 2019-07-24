7bf50a3e28aa2fd6dfea4fc9d041854bb6546693
Mateusz Jezierski <jezierski.mat@gmail.com>
2019-07-24 11:53:09 +0200

    implements searching in 'explore'

--- src/main/java/pl/matshead/Gifs/controller/HomeController.java
+++ src/main/java/pl/matshead/Gifs/controller/HomeController.java
@@ -4,8 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import pl.matshead.Gifs.Gif;
 import pl.matshead.Gifs.repositories.GifRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Controller
@@ -15,8 +19,20 @@ public class HomeController {
     private GifRepository gifRepository;
 
     @GetMapping("/")
-    public String home(ModelMap modelMap){
-        modelMap.put("paths", gifRepository.getGifs());
+    public String home(@RequestParam(required = false) String q, ModelMap modelMap){
+        if (q != null) {
+            List<Gif> paths = new ArrayList<>();
+            Gif gif = Gif.getGifByName(q, gifRepository.getGifs());
+            if (gif != null) {
+                paths.add(gif);
+                modelMap.put("paths", paths);
+            } else {
+                modelMap.put("paths", null);
+            }
+
+        } else {
+            modelMap.put("paths", gifRepository.getGifs());
+        }
         return "home";
     }
 }
